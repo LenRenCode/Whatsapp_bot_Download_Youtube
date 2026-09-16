f46c6d55313d58cd9cfa802f22244d1cd036ceb0
LinRenCode <ahmadsuryags@gmail.com>
2026-09-16 23:42:01 +0800

    Create Package.js
    memasang package

---
+++ Package.js
@@ -0,0 +1,14 @@
+{
+  "name": "wa-media-bot",
+  "version": "1.0.0",
+  "description": "WhatsApp bot that downloads video/music via yt-dlp (up to 1080p)",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.7.9",
+    "pino": "^9.3.2",
+    "qrcode-terminal": "^0.12.0"
+  }
+}
